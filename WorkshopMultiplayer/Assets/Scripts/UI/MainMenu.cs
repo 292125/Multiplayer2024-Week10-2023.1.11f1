6ddaedf6781b66afe9ad836c2a5b69b65154b802
292125 <85726099+292125@users.noreply.github.com>
2024-03-14 10:42:19 +0700

    week10

--- WorkshopMultiplayer/Assets/Scripts/UI/MainMenu.cs
+++ WorkshopMultiplayer/Assets/Scripts/UI/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,6 +7,14 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
    [SerializeField] private TMP_InputField joinCodeField;
+
+   private void Start()
+   {
+       if (ClientSingleton.Instance == null) { return; }
+       
+       Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+   }
+
    public async void StartHost()
    {
         await HostSingleton.Instance.GameManager.StartHostAsync();
